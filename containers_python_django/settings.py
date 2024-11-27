e0cb5e61b46ca06c203ead5b8431d4b8b46d80ed
TheSpaceSheep <noe.malais@gmail.com>
2024-11-27 15:44:42 +0100

    pass env to settings

--- containers_python_django/settings.py
+++ containers_python_django/settings.py
@@ -123,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+MY_ENV_VARIABLE = os.getenv('MY_ENV_VARIABLE', 'Variable not set')
--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 import os
 from django.http import JsonResponse
+from django.conf import settings
 
 # Create your views here.
 def home(request):
@@ -9,12 +10,11 @@ def home(request):
 
 def get_fixed_content_with_env(request):
     # Access the environment variable
-    my_var = os.getenv('MY_ENV_VARIABLE', 'Variable not set')
     
     # Create a fixed content response combined with the environment variable
     response_content = {
         'fixed_message': 'This is a fixed message.',
-        'env_variable': my_var  # Include the environment variable
+        'env_variable': settings.MY_ENV_VARIABLE  # Include the environment variable
     }
     
     # Return the response as JSON
